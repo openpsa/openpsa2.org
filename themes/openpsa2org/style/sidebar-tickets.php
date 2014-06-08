62df59c5fe62b6b0bfc69f928103c7eb56646a0c
flack <flack@contentcontrol-berlin.de>
2014-06-08 09:42:20 +0200

    Adapt to SimplePie API

--- themes/openpsa2org/style/sidebar-tickets.php
+++ themes/openpsa2org/style/sidebar-tickets.php
@@ -4,15 +4,15 @@ $rss = net_nemein_rss_fetch::raw_fetch('http://trac.openpsa2.org/query?status=ne
 echo '<h3><a href="http://trac.openpsa2.org/query?status=new&status=assigned&status=reopened&group=type&order=priority">Open tickets</a></h3>';
 echo "<ul class='tickets'>\n";
 
-foreach ($rss->items as $i)
+foreach ($rss->get_items() as $i)
 {
     echo "<li>\n";
-    $text = $i['title'];
+    $text = $i->get_title();
     if (strlen($text) > 73)
     {
         $text = substr($text, 0, 70) . '...';
     }
-    echo '<a href="' . $i['link'] . '">' . $text . "</a><br />";
+    echo '<a href="' . $i->get_link() . '">' . $text . "</a><br />";
     echo "</li>\n";
 }
 echo "</ul>\n";
