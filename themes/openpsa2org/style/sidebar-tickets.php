1e814a3fe805d7ddb663d37f8fc76ab179a94455
flack <flack@contentcontrol-berlin.de>
2016-06-05 00:09:05 +0200

    Remove defunct trac integration & point to gh instead

--- themes/openpsa2org/style/sidebar-tickets.php
+++ themes/openpsa2org/style/sidebar-tickets.php
@@ -1,8 +1,8 @@
 <?php
-$rss = net_nemein_rss_fetch::raw_fetch('http://trac.openpsa2.org/query?status=new&status=assigned&status=reopened&format=rss&order=priority');
+$rss = net_nemein_rss_fetch::raw_fetch('https://github.com/flack/openpsa/commits/master.atom');
 
-echo '<h3><a href="http://trac.openpsa2.org/query?status=new&status=assigned&status=reopened&group=type&order=priority">Open tickets</a></h3>';
-echo "<ul class='tickets'>\n";
+echo '<h3><a href="https://github.com/flack/openpsa/commits">Recent Changes</a></h3>';
+echo "<ul class='commits'>\n";
 
 foreach ($rss->get_items() as $i)
 {
@@ -12,9 +12,10 @@ foreach ($rss->get_items() as $i)
     {
         $text = substr($text, 0, 70) . '...';
     }
-    echo '<a href="' . $i->get_link() . '">' . $text . "</a><br />";
+    echo '<a href="' . $i->get_link() . '"><span class="timestamp">' . $i->get_date() . '</span>' . $text . "</a><br />";
     echo "</li>\n";
 }
 echo "</ul>\n";
 ?>
+<br>
 <script type="text/javascript" src="http://www.ohloh.net/p/486313/widgets/project_partner_badge.js"></script>
